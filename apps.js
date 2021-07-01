a073b74aa57eeb5da268f4372120b8d66041a6ae
DinaAlamouri <>
2021-07-01 22:32:02 +0000

    initail code js

---
+++ apps.js
@@ -0,0 +1,16 @@
+var name = prompt ("what is your name?")
+console.log(name) 
+alert("Welcome to the movieworld "  + name)
+
+var favMovie = prompt("what is your favGenre from movies?              action,comedy,adventure,drama,romance,all of above")
+ 
+  if (favMovie == null)
+  
+   {alert ("please input answer")}
+ 
+ var favMovie = prompt("what is your favGenre from movies?              action,comedy,adventure,drama,romance,all of above") 
+ 
+ var x = ("Great choice! You will find all kinds of movies you want on this website.") 
+if (favMovie !=null)
+{document.write ("<h4>" + x + "</h4>" )}
+
