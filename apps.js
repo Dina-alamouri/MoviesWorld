ea3b2915a994cdbcc54aa9599973b02d4ea57a34
DinaAlamouri <>
2021-07-03 17:02:42 +0000

    loops add

--- apps.js
+++ apps.js
@@ -12,5 +12,30 @@ var favMovie = prompt("what is your favGenre from movies?              action,co
  
  var x = ("Great choice! You will find all kinds of movies you want on this website.") 
 if (favMovie !=null)
-{document.write ("<h4>" + x + "</h4>" )}
+{document.write ("<h4>" + x + "</h4>" )} 
+
+var r = prompt("Approximately ,how many time have you watched the same movie? ")
+
+if (r >= 1 && r <=10 ){
+  alert("That is great , It sure is a nice movie")
+
+ }
+
+else{
+   alert("that is so many times, you seem to be a top fan ")
+}
+
+var watch =prompt("what you have watched form these movies : the old guard,6 underground,joker,luca")
+
+
+var time=prompt("how many times do you want to watch it again? ")
+
+for(var i = 0 ; i < time; i++)
+{
+
+document.write("<div>" + "<h4>" + watch +"</h4>")
+
+
+}
+alert("Itis a joke!")
 
